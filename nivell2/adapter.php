2061cde9b2b14b1369f244ef7bab8eb41343047f
gfarrusg <113913547+gfarrusg@users.noreply.github.com>
2022-10-25 09:14:51 +0200

    first impressions

--- nivell2/adapter.php
+++ nivell2/adapter.php
@@ -1,1 +1,27 @@
+<?php
 
+class Duck {
+
+    public function quack() {
+       echo "Quack \n";
+    }
+
+    public function fly() {
+       echo "I'm flying \n";
+    }
+}
+
+class Turkey {
+
+    public function gobble() {
+       echo "Gobble gobble \n";
+    }
+
+    public function fly() {
+        echo "I'm flying a short distance \n";
+    }
+}
+
+    
+
+?>
